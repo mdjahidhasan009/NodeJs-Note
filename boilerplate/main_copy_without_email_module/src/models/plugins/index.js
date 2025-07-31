cea533f5e620ff7cf33a0ea42697b202686c7448
mdjahidhasan009 <jahid60012@gmail.com>
2025-07-31 18:29:32 +0600

    refactoring

---
+++ boilerplate/main_copy_without_email_module/src/models/plugins/index.js
@@ -0,0 +1,5 @@
+// module.exports.toJSON = require('./toJSON.plugin');
+// module.exports.paginate = require('./paginate.plugin');
+
+export { default as toJSON } from './toJSON.plugin.js';
+export { default as paginate } from './paginate.plugin.js';
